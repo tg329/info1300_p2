32ae89888775ce007295ce7e3b64c177652ac3a8
Jolly Zheng <jz767@cornell.edu>
2024-11-16 15:56:18 -0500

    finished data cleaning

--- new/filters/populations/merge_years.py
+++ new/filters/populations/merge_years.py
@@ -4,10 +4,9 @@ path_1 = '00-09/00-09.csv'
 path_2 = '10-19/10-19.csv'
 path_3 = '20-23/20-23.csv'
 
-df1 = pd.read_csv(path_1, delimiter=',')
-df2 = pd.read_csv(path_2, delimiter=',')
-df3 = pd.read_csv(path_3, delimiter=',')
+df1 = pd.read_csv(path_1, delimiter=',', thousands=',')
+df2 = pd.read_csv(path_2, delimiter=',', thousands=',')
+df3 = pd.read_csv(path_3, delimiter=',', thousands=',')
 
-final_df = pd.concat([df1, df2,df3], ignore_index=True)
-final_df.to_csv('final.csv', index=False)
-
+final_df = df1.merge(df2, on='States', how='outer').merge(df3, on='States', how='outer')
+final_df.to_csv('pop_all_years.csv', index=False)
