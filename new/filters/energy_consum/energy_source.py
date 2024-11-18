ebae1596d6cb85de6b4c762d7f02181c094ebaa6
Jolly Zheng <jz767@cornell.edu>
2024-11-18 00:01:11 -0500

    changes to source

--- new/filters/energy_consum/energy_source.py
+++ new/filters/energy_consum/energy_source.py
@@ -1,15 +1,19 @@
 import pandas as pd
 
 file_path = 'organised_Gen.csv'
+state_path = 'states.csv'
 energy_df = pd.read_csv(file_path, delimiter=',')
+states_df = pd.read_csv(state_path, delimiter=',')
 energy_df = energy_df[energy_df['ENERGY SOURCE'] != 'Total']
 
 aggregated_data = energy_df.groupby(
-    ['YEAR', 'ENERGY SOURCE']
+    ['STATE', 'YEAR', 'ENERGY SOURCE']
 )['GENERATION (Megawatthours)'].sum().reset_index()
 
-new_data = [['YEAR', 'ENERGY SOURCE', 'GENERATION (Megawatthours)']] + aggregated_data.values.tolist()
+states_df = states_df.rename(columns={'Code': 'STATE'})
 
-yearly_df = pd.DataFrame(new_data[1:], columns=new_data[0])
+yearly_df = states_df.merge(aggregated_data, on = 'STATE', how = 'left')
+yearly_df.drop('Abbrev', axis=1, inplace=True)
+yearly_df.drop('STATE', axis=1, inplace=True)
 
 yearly_df.to_csv('source_years.csv', index=False)
